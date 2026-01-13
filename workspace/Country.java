6f1b140fd81d52b04e55183501dfbdfbcdb2eec7
melinozturk <MelinOzturk0707@gmail.com>
2026-01-13 15:57:27 +0000

    jaskalkdjasdlkj

--- workspace/Country.java
+++ workspace/Country.java
@@ -1,8 +1,19 @@
+//Melin Ozturk
+//1/13/2026
+// This is the Country class to make the main game work
+
 public class Country
 {
   // add private instance variables for the name, capital, language, and image file.
 
+  private String name;
+  private String capital;
+  private Sting language;
+  private imageFile; 
+
   // add constructors
+
+  public name (String name)
 
   // Write accessor/get methods for each instance variable that returns it.
 
