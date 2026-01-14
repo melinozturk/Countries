662b151717c4e4e9fdb3a9515a57b8b88bc21c64
melinozturk <MelinOzturk0707@gmail.com>
2026-01-14 15:47:28 +0000

    kaasdklkasdoa[dijaopfajskalf;kslsd

--- workspace/Country.java
+++ workspace/Country.java
@@ -35,9 +35,6 @@ public class Country
   // Write a toString() method that returns a concatenated String of 3 of the instance variables in a sentence like "..'s capital is .. and its primary language is ..."
  public String toString(){
     return name + "'s capital is " + capital + " and its primary language is " + language + ".";
-  }
-
-
-
+ }
   
 }
